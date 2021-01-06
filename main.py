3e52840a7f58f114136bd0f76e204424107c94af
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2021-01-06 19:58:08 +0200

    score function + small adjustments

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ import os
 from io import BytesIO
 from random import randrange
 from youtubesearchpython import SearchVideos
-
+from PIL import UnidentifiedImageError
+import webbrowser
 
 #connecting to database
 
@@ -17,20 +18,23 @@ conn = sqlite3.connect('shows.db')
 c = conn.cursor()
 
 #created tabel
-# c.execute(""" CREATE TABLE tv_shows(
-#    id VARCHAR(255) UNIQUE,
-#    name VARCHAR(255) UNIQUE,
-#    link VARCHAR(255) NOT NULL,
-#    last_season INTEGER,
-#    last_episode INTEGER,
-#    user_season INTEGER,
-#    user_episode INTEGER,
-#    last_watch VARCHAR(255),
-#    score DECIMAL(3,2),
-#    trailer_link VARCHAR(255),
-#    image_link VARCHAR(255)
-# ) 
-#    """)
+c.execute(""" CREATE TABLE tv_shows(
+   id VARCHAR(255) UNIQUE,
+   name VARCHAR(255) UNIQUE,
+   link VARCHAR(255) NOT NULL,
+   last_season INTEGER,
+   last_episode INTEGER,
+   user_season INTEGER,
+   user_episode INTEGER,
+   last_watch VARCHAR(255),
+   score FLOAT,
+   trailer_link VARCHAR(255),
+   image_link VARCHAR(255),
+   finished BIT,
+   snoozed BIT,
+   rated_episodes INTEGER
+) 
+   """)
 
 conn.commit()
 
@@ -78,6 +82,7 @@ def searchResults(obj, widget1, widget2, widget3):
     for widget in root.winfo_children():
         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != widget1 and widget != widget2 and widget != widget3:
             widget.destroy()
+    photoimage_list.clear()
     name = []
     fav_buttons= []
     show_name = obj.get()
@@ -92,15 +97,20 @@ def searchResults(obj, widget1, widget2, widget3):
     for index in range(0,displayedResults):
         name.append(json_data['results'][index]['name'])
         img_url = 'https://image.tmdb.org/t/p/w500' + str(json_data['results'][index]['poster_path'])
-        debut = str(json_data['results'][index]['first_air_date'])
+        debut = str(json_data['results'][index]['first_air_date'])  
+        try:
+            img_data = requests.get(img_url).content
+            img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+            img = img._PhotoImage__photo.subsample(3)
+            if img != 'https://image.tmdb.org/t/p/w500None':
+                photoimage_list.append(img)
         
-        img_data = requests.get(img_url).content
-        img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-        img = img._PhotoImage__photo.subsample(3)
-        photoimage_list.append(img)
+        except UnidentifiedImageError:
+            print(img_url)
+
         panel = Label(root, image=img)
         
-        if index < 5:  
+        if index < 5:
             panel.place(x = 350 + row_space * index , y = 250)
             fav_buttons.append(Button(root, command = lambda c = index: add_favorites(json_data, c, name[c]), text = "Add to Favorites", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
             fav_buttons[index].place(x = 380 + row_space * index, y = 520)
@@ -109,10 +119,12 @@ def searchResults(obj, widget1, widget2, widget3):
             fav_buttons.append(Button(root, command = lambda c = index: add_favorites(json_data, c,name[c]), text = "Add to Favorites", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
             fav_buttons[index].place(x = 380 + row_space * (index-5), y = 870)
 
+    for pic in photoimage_list:
+        print(pic)
+
 
 
 def searchWindow():
-    print('search')
     for widget in root.winfo_children():
          if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
             widget.destroy()
@@ -122,34 +134,12 @@ def searchWindow():
     input.place(relx=0.55, rely=0.15, anchor=CENTER)
     submitButton = Button(root, text="Submit", width=10, command = lambda: searchResults(input, text, input, submitButton), font = ('Verdana', 16), bg = '#696969')
     submitButton.place(relx = 0.76, rely = 0.15, anchor = CENTER)
-
-
-def add_favorites(obj, id, name):
     
-    results = SearchVideos(name + ' Official Trailer', offset = 1, mode = "dict", max_results = 1).result()
-    trailer_link = results['search_result'][0]['link']
-    tv_index = str(obj['results'][id]['id'])
-    imdb_link = "https://www.imdb.com/title/tt"+tv_index+"/?ref_=nv_sr_srsg_0"
-    img_url = 'https://image.tmdb.org/t/p/w500' + obj['results'][id]['poster_path']
+def callback(url):
+    webbrowser.open_new(url)
 
-    response = requests.get("https://api.themoviedb.org/3/tv/"+ tv_index +"?api_key=ead60b2309bd3aba9817000af517c069&language=en-US")
-    json_tv_data = json.loads(response.text)
-    
-    last_season_to_air = json_tv_data['last_episode_to_air']['season_number']
-    last_episode_to_air = json_tv_data['last_episode_to_air']['episode_number']
-    c.execute(""" INSERT OR IGNORE INTO tv_shows VALUES
-        (?, ?, ?, ?, ?, 1, 0, NULL, NULL, ?, ?)
 
-    """, (tv_index, name, imdb_link, last_season_to_air, last_episode_to_air, trailer_link, img_url))
-    conn.commit()
-
-    c.execute("SELECT * FROM tv_shows")
-    print(c.fetchall())
-    print('\n')
-    
-
-def _show(data, season, episode):  
-    
+def _show(data, season, episode, flag):  
     for widget in root.winfo_children():
          if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
             widget.destroy()
@@ -164,7 +154,7 @@ def _show(data, season, episode):
 
     time_date = data[7]
 
-    if episode != 0:
+    if flag:
         time_date = str(date.today())
 
     c = conn.cursor()
@@ -184,28 +174,108 @@ def _show(data, season, episode):
     img = img._PhotoImage__photo.subsample(2)
     photoimage_list.append(img)
     panel = Label(root, image = img)
-    panel.place(x = 840, y = 80)
+    panel.place(x = 840, y = 110)
+
     show_length = Label(root, text = 'Seasons: {}  Episodes: {}'.format(data[3], data[4]))
-    show_length.place(x = 900, y  = 470)
+    show_length.place(x = 900, y  = 500)
 
 
     if last_viewed_episode == int(data[4]) + 1:
         last_viewed_season = season + 1
         last_viewed_episode = 1
-
+    
     if season == int(data[3]) and episode == int(data[4]):
         mark_button = Button(root, text = "You've finished the show", command = lambda: favorites(), width = 28, height = 3, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 12), activebackground = '#FFCB09', activeforeground = "#000000")
-        mark_button.place(x = 835, y = 500)
-    else:
-        mark_button = Button(root, text = "I've seen Episode {} of Season {}".format(last_viewed_episode, last_viewed_season), command = lambda: _show(data, last_viewed_season, last_viewed_episode), width = 28, height = 3, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 12), activebackground = '#FFCB09', activeforeground = "#000000")
-        mark_button.place(x = 835, y = 500)
-    
-    if last_viewed_episode > 1  and last_viewed_season == 1:
-        last_time_watched =  Label(root, text = 'Last time you watched {} was on {}'.format(data[1], data[7]))
-        last_time_watched.place(x = 800, y = 580)
+        c = conn.cursor()
+        c.execute(""" UPDATE tv_shows SET finished = 1 WHERE id LIKE ? """, (data[0],))
+        conn.commit()
+        c.close()  
+        mark_button.place(relx = 0.435, y = 540)
 
+        last_time_watched =  Label(root, text = 'Last time you watched {} was on {}'.format(data[1], data[7]), font = ('sans-serif', 12) )
+        last_time_watched.configure(anchor = CENTER)
+        last_time_watched.pack()
+    else:
+        mark_button = Button(root, text = "Next: Episode {} of Season {}".format(last_viewed_episode, last_viewed_season), command = lambda: _show(data, last_viewed_season, last_viewed_episode, flag = True), width = 28, height = 3, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 12), activebackground = '#FFCB09', activeforeground = "#000000")
+        mark_button.place(relx = 0.435, y = 540)
+
+    if last_viewed_episode > 1:
+        current_episode =  Label(root, text = """ You're currently at Episode: {} Season: {} """.format(data[6], data[5]), font = ('sans-serif', 12) )
+        current_episode.configure(anchor = CENTER)
+        current_episode.pack()
+
+        last_time_watched =  Label(root, text = 'Last time you watched {} was on {}'.format(data[1], data[7]), font = ('sans-serif', 12) )
+        last_time_watched.configure(anchor = CENTER)
+        last_time_watched.pack()
+
+    hyperlinkTrailer = Label(root, text = 'Trailer', bg ='#FF0000', fg = '#FFFFFF', font = ('sans-serif', 15, 'bold'), padx = 12, pady = 6)
+    hyperlinkTrailer.place(relx = 0.45, y = 640)
+    hyperlinkTrailer.bind('<Button-1>', lambda e: callback(data[9]))
+
+    hyperlinkTMDB = Label(root, text = 'TMDB', bg ='#01b4e4', fg = '#FFFFFF', font = ('sans-serif', 15, 'bold'), padx = 12, pady = 6)
+    hyperlinkTMDB.place(relx = 0.51, y = 640)
+    hyperlinkTMDB.bind('<Button-1>', lambda e: callback(data[2] + '/season/' + str(data[5]) + '/episode/' + str(data[6])))
+
+    def update_score(id, rating):
+        print('Rating: ', rating)
+        
+        c = conn.cursor()
+        c.execute(""" UPDATE tv_shows SET rated_episodes = rated_episodes + 1 WHERE id LIKE ? """, (id,))
+        conn.commit()
+
+        c.execute(""" SELECT  rated_episodes from tv_shows WHERE id LIKE ? """, (id,))
+        string = ''.join(str(x) for x in c.fetchall())
+        rated_episodes = int(string[1])
+
+        c.execute(""" SELECT score from tv_shows WHERE id LIKE ? """, (id,))
+        string =''.join(str(x) for x in c.fetchall())
+        score = float(string[1] + string[2] + string[3])
+        
+        if score != 0.0:
+            updated_score = (score + int(rating)) / 2
+        else:
+            updated_score = (score + int(rating))
+        c.execute(""" UPDATE tv_shows SET score = ? WHERE id LIKE ? """, (updated_score, id))
+        print('episodes: ', rated_episodes)
+        print(updated_score)
+        conn.commit()
+        c.close()  
+
+       
+
+    rating = Entry(root, width = 5, font = ('Verdana', 16), fg = '#696969')
+    rating.place(x = 910, y = 720)
+
+    submitButton = Button(root, text="Rate", command = lambda: update_score(data[0],rating.get()) , width=5, font = ('Verdana', 10), bg = '#696969')
+    submitButton.place(x = 985,y = 720)
 
     c.close()
+
+
+def add_favorites(obj, id, name):
+    
+    results = SearchVideos(name + ' Official Trailer', offset = 1, mode = "dict", max_results = 1).result()
+    trailer_link = results['search_result'][0]['link']
+    tv_index = str(obj['results'][id]['id'])
+    tmdb_link = "https://www.themoviedb.org/tv/"+tv_index
+    img_url = 'https://image.tmdb.org/t/p/w500' + obj['results'][id]['poster_path']
+
+    response = requests.get("https://api.themoviedb.org/3/tv/"+ tv_index +"?api_key=ead60b2309bd3aba9817000af517c069&language=en-US")
+    json_tv_data = json.loads(response.text)
+    
+    last_season_to_air = json_tv_data['last_episode_to_air']['season_number']
+    last_episode_to_air = json_tv_data['last_episode_to_air']['episode_number']
+    c.execute(""" INSERT OR IGNORE INTO tv_shows VALUES
+        (?, ?, ?, ?, ?, 1, 0, NULL, 0, ?, ?, False, False, 0)
+
+    """, (tv_index, name, tmdb_link, last_season_to_air, last_episode_to_air, trailer_link, img_url))
+    conn.commit()
+
+    c.execute("SELECT * FROM tv_shows")
+    print(c.fetchall())
+    print('\n')
+
+
 def favorites():
     for widget in root.winfo_children():
          if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
@@ -217,7 +287,7 @@ def favorites():
 
 
     c = conn.cursor()
-    row = c.execute("select * from tv_shows")
+    row = c.execute("select * from tv_shows where finished = 0")
     rows = c.fetchall()
     endline = 0
     photoimage_list.clear() 
@@ -234,23 +304,26 @@ def favorites():
     iterate = 0
 
     for row in rows:
-        img_url = row[10]
-        img_data = requests.get(img_url).content
-        img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-        img = img._PhotoImage__photo.subsample(3)
-        photoimage_list.append(img)
-
-        panel = Label(canvas, image = img)
         
-        if(endline > 4):
-            vertical_pos = vertical_pos + 340
-            endline = 0
+        if row[11] == 0:
+            
+            img_url = row[10]
+            img_data = requests.get(img_url).content
+            img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+            img = img._PhotoImage__photo.subsample(3)
+            photoimage_list.append(img)
 
-        canvas.create_window((30 + row_space * endline,240+ vertical_pos), window = panel, anchor = 'nw')
-        open_buttons.append(Button(canvas, command = lambda c = iterate: _show(rows[c], rows[c][5], rows[c][6]), text = "Open Show", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
-        canvas.create_window((60 + row_space * endline, 515 + vertical_pos), window = open_buttons[iterate], anchor = 'nw')  
-        iterate += 1
-        endline += 1
+            panel = Label(canvas, image = img)
+            
+            if(endline > 4):
+                vertical_pos = vertical_pos + 340
+                endline = 0
+
+            canvas.create_window((30 + row_space * endline,240+ vertical_pos), window = panel, anchor = 'nw')
+            open_buttons.append(Button(canvas, command = lambda c = iterate: _show(rows[c], rows[c][5], rows[c][6], flag = False), text = "Open Show", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
+            canvas.create_window((60 + row_space * endline, 515 + vertical_pos), window = open_buttons[iterate], anchor = 'nw')  
+            iterate += 1
+            endline += 1
 
     canvas.configure(yscrollcommand = scrollbar.set)
     canvas.bind('<Configure>', on_configure)
