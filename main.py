423104861cb88aa48e369546237e5bf45eb09e2b
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2021-01-08 22:02:48 +0200

    snooze feature added

--- main.py
+++ main.py
@@ -18,23 +18,22 @@ conn = sqlite3.connect('shows.db')
 c = conn.cursor()
 
 #created tabel
-c.execute(""" CREATE TABLE tv_shows(
-   id VARCHAR(255) UNIQUE,
-   name VARCHAR(255) UNIQUE,
-   link VARCHAR(255) NOT NULL,
-   last_season INTEGER,
-   last_episode INTEGER,
-   user_season INTEGER,
-   user_episode INTEGER,
-   last_watch VARCHAR(255),
-   score FLOAT,
-   trailer_link VARCHAR(255),
-   image_link VARCHAR(255),
-   finished BIT,
-   snoozed BIT,
-   rated_episodes INTEGER
-) 
-   """)
+# c.execute(""" CREATE TABLE tv_shows(
+#    id VARCHAR(255) UNIQUE,
+#    name VARCHAR(255) UNIQUE,
+#    link VARCHAR(255) NOT NULL,
+#    last_season INTEGER,
+#    last_episode INTEGER,
+#    user_season INTEGER,
+#    user_episode INTEGER,
+#    last_watch VARCHAR(255),
+#    score FLOAT,
+#    trailer_link VARCHAR(255),
+#    image_link VARCHAR(255),
+#    finished BIT,
+#    snoozed BIT
+# ) 
+#    """)
 
 conn.commit()
 
@@ -51,16 +50,19 @@ def exit_ (root):
     root.destroy()
 
 search_button = Button(root, text = "Search", command = lambda: searchWindow(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
-search_button.place(x = 40, y = root.winfo_screenheight() *  0.20  - 50)
+search_button.place(x = 40, y = root.winfo_screenheight() *  0.10  - 50)
 
-discover_button = Button(root, text = "Discover", command = lambda: home_page(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
-discover_button.place(x = 40, y = root.winfo_screenheight() *  0.40 - 50)
+discover_button = Button(root, text = "Trending", command = lambda: home_page(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
+discover_button.place(x = 40, y = root.winfo_screenheight() *  0.30 - 50)
 
-favorite_button = Button(root, text = "Favorite", command = lambda: favorites(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
-favorite_button.place(x = 40, y = root.winfo_screenheight() * 0.60 - 50)
+favorite_button = Button(root, text = "Favorites", command = lambda: favorites(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
+favorite_button.place(x = 40, y = root.winfo_screenheight() * 0.50 - 50)
+
+collection_button = Button(root, text = "Snoozed shows", command = lambda: show_collection(), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000" )
+collection_button.place(x = 40, y = root.winfo_screenheight() * 0.70 - 50)
 
 exit_button = Button(root, text = "Exit", command = lambda: exit_(root), width = 20, height = 5, bg = '#000000', fg = '#FFCB09', font = 'sans-serif', activebackground = '#FFCB09', activeforeground = "#000000")
-exit_button.place(x = 40, y = root.winfo_screenheight() * 0.80 - 50)
+exit_button.place(x = 40, y = root.winfo_screenheight() * 0.90 - 50)
 
 def jprint(obj):
     # create a formatted string of the Python JSON object
@@ -97,7 +99,6 @@ def searchResults(obj, widget1, widget2, widget3):
     for index in range(0,displayedResults):
         name.append(json_data['results'][index]['name'])
         img_url = 'https://image.tmdb.org/t/p/w500' + str(json_data['results'][index]['poster_path'])
-        debut = str(json_data['results'][index]['first_air_date'])  
         try:
             img_data = requests.get(img_url).content
             img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
@@ -126,7 +127,7 @@ def searchResults(obj, widget1, widget2, widget3):
 
 def searchWindow():
     for widget in root.winfo_children():
-         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
+         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != collection_button:
             widget.destroy()
     text = Label(root, text = 'Type a TV Show: ', font = ('Verdana', 25), bg = '#696969')
     text.place(relx=0.30,rely=0.15, anchor = CENTER)
@@ -139,9 +140,61 @@ def callback(url):
     webbrowser.open_new(url)
 
 
+def show_collection():
+
+    for widget in root.winfo_children():
+        if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != collection_button:
+            widget.destroy()
+
+    c = conn.cursor()
+    c.execute('SELECT * FROM tv_shows where snoozed = 1')
+    
+    rows = c.fetchall()
+
+    canvas = Canvas(root, width = root.winfo_screenwidth() , height = root.winfo_screenheight() -25, bg = background_color, bd = 0, highlightthickness=0)
+    canvas.place(x = 325, y=20)
+
+    scrollbar = Scrollbar(root, command=canvas.yview)
+    scrollbar.pack(side='right', fill = 'y')
+
+    def on_configure(event):  
+        canvas.configure(scrollregion=canvas.bbox('all'))
+
+    endline = 0
+    iterate = 0
+    vertical_pos = 0
+    unsnooze_buttons = []
+
+    for row in rows:
+        img_data = requests.get(row[10]).content
+        img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+        img = img._PhotoImage__photo.subsample(3)
+        photoimage_list.append(img)
+
+        panel = Label(canvas, image = img)
+        
+        if(endline > 4):
+            vertical_pos = vertical_pos + 340
+            endline = 0
+
+        canvas.create_window((30 + row_space * endline,240+ vertical_pos), window = panel, anchor = 'nw')
+
+        unsnooze_buttons.append(Button(canvas, command = lambda c = iterate : unsnooze(rows[c]), text = 'Unsnooze', bg ='#554014'))
+        canvas.create_window((200 + row_space * endline, 240 + vertical_pos), window = unsnooze_buttons[iterate], anchor = 'nw')
+
+        iterate += 1
+        endline += 1
+
+
+    canvas.configure(yscrollcommand = scrollbar.set)
+    canvas.bind('<Configure>', on_configure)
+
+
+
+
 def _show(data, season, episode, flag):  
     for widget in root.winfo_children():
-         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
+         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != collection_button:
             widget.destroy()
 
     title = Label(root, text = data[1], bg = background_color, fg = '#FFCB09', font = ('sans-serif', 40), highlightcolor = '#FFF')
@@ -216,18 +269,14 @@ def _show(data, season, episode, flag):
     hyperlinkTMDB.place(relx = 0.51, y = 640)
     hyperlinkTMDB.bind('<Button-1>', lambda e: callback(data[2] + '/season/' + str(data[5]) + '/episode/' + str(data[6])))
 
-    def update_score(id, rating):
+    def update_score(id, rating, widget):
+        widget.destroy()
+        
         print('Rating: ', rating)
         
         c = conn.cursor()
-        c.execute(""" UPDATE tv_shows SET rated_episodes = rated_episodes + 1 WHERE id LIKE ? """, (id,))
-        conn.commit()
-
-        c.execute(""" SELECT  rated_episodes from tv_shows WHERE id LIKE ? """, (id,))
-        string = ''.join(str(x) for x in c.fetchall())
-        rated_episodes = int(string[1])
-
         c.execute(""" SELECT score from tv_shows WHERE id LIKE ? """, (id,))
+        conn.commit()
         string =''.join(str(x) for x in c.fetchall())
         score = float(string[1] + string[2] + string[3])
         
@@ -236,18 +285,26 @@ def _show(data, season, episode, flag):
         else:
             updated_score = (score + int(rating))
         c.execute(""" UPDATE tv_shows SET score = ? WHERE id LIKE ? """, (updated_score, id))
-        print('episodes: ', rated_episodes)
-        print(updated_score)
+
         conn.commit()
-        c.close()  
-
-       
-
+        c.close()
+        score_box = Label(root, text = updated_score, bg = '#E09500', font = ('Verdana', 32, 'bold'), width = 5, pady = 15)
+        score_box.place(x = 1720, y= 0)
+   
+        
     rating = Entry(root, width = 5, font = ('Verdana', 16), fg = '#696969')
     rating.place(x = 910, y = 720)
 
-    submitButton = Button(root, text="Rate", command = lambda: update_score(data[0],rating.get()) , width=5, font = ('Verdana', 10), bg = '#696969')
+    submitButton = Button(root, text="Rate", command =  lambda: update_score(data[0],rating.get(), score_box) , width=5, font = ('Verdana', 10), bg = '#696969')
     submitButton.place(x = 985,y = 720)
+    
+    c = conn.cursor()
+    c.execute(""" SELECT score from tv_shows WHERE id LIKE ? """, (data[0],))
+    string = ''.join(str(x) for x in c.fetchall())
+    score = float(string[1] + string[2] + string[3])
+    
+    score_box = Label(root, text = score, bg = '#E09500', font = ('Verdana', 32, 'bold'), pady = 15, width = 5)
+    score_box.place(x = 1720, y= 0)
 
     c.close()
 
@@ -263,10 +320,11 @@ def add_favorites(obj, id, name):
     response = requests.get("https://api.themoviedb.org/3/tv/"+ tv_index +"?api_key=ead60b2309bd3aba9817000af517c069&language=en-US")
     json_tv_data = json.loads(response.text)
     
+    print(json_tv_data['last_episode_to_air']['season_number'])
     last_season_to_air = json_tv_data['last_episode_to_air']['season_number']
     last_episode_to_air = json_tv_data['last_episode_to_air']['episode_number']
     c.execute(""" INSERT OR IGNORE INTO tv_shows VALUES
-        (?, ?, ?, ?, ?, 1, 0, NULL, 0, ?, ?, False, False, 0)
+        (?, ?, ?, ?, ?, 1, 0, NULL, 0, ?, ?, False, 0)
 
     """, (tv_index, name, tmdb_link, last_season_to_air, last_episode_to_air, trailer_link, img_url))
     conn.commit()
@@ -274,11 +332,28 @@ def add_favorites(obj, id, name):
     c.execute("SELECT * FROM tv_shows")
     print(c.fetchall())
     print('\n')
+    c.close()
+
+
+
+def snooze(data):
+    c = conn.cursor()
+    c.execute("UPDATE tv_shows SET snoozed = 1 where id = ?", (data[0],))
+    conn.commit()
+    c.close()
+    favorites()
+
+def unsnooze(data):
+    c = conn.cursor()
+    c.execute("UPDATE tv_shows SET snoozed = 0 where id = ?", (data[0],))
+    conn.commit()
+    c.close()
+    show_collection()
 
 
 def favorites():
     for widget in root.winfo_children():
-         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
+         if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != collection_button:
             widget.destroy()
 
 
@@ -287,12 +362,13 @@ def favorites():
 
 
     c = conn.cursor()
-    row = c.execute("select * from tv_shows where finished = 0")
+    row = c.execute("select * from tv_shows where finished = 0 and snoozed = 0 order by score desc")
     rows = c.fetchall()
     endline = 0
     photoimage_list.clear() 
     vertical_pos = 0
     open_buttons = []
+    snooze_buttons = []
     
 
     canvas = Canvas(root, width = root.winfo_screenwidth() , height = root.winfo_screenheight() -25, bg = background_color, bd = 0, highlightthickness=0)
@@ -303,27 +379,30 @@ def favorites():
    
     iterate = 0
 
-    for row in rows:
+    for row in rows:    
+        img_url = row[10]
+        img_data = requests.get(img_url).content
+        img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+        img = img._PhotoImage__photo.subsample(3)
+        photoimage_list.append(img)
+
+        panel = Label(canvas, image = img)
         
-        if row[11] == 0:
-            
-            img_url = row[10]
-            img_data = requests.get(img_url).content
-            img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-            img = img._PhotoImage__photo.subsample(3)
-            photoimage_list.append(img)
+        if(endline > 4):
+            vertical_pos = vertical_pos + 340
+            endline = 0
 
-            panel = Label(canvas, image = img)
-            
-            if(endline > 4):
-                vertical_pos = vertical_pos + 340
-                endline = 0
+        canvas.create_window((30 + row_space * endline,240+ vertical_pos), window = panel, anchor = 'nw')
 
-            canvas.create_window((30 + row_space * endline,240+ vertical_pos), window = panel, anchor = 'nw')
-            open_buttons.append(Button(canvas, command = lambda c = iterate: _show(rows[c], rows[c][5], rows[c][6], flag = False), text = "Open Show", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
-            canvas.create_window((60 + row_space * endline, 515 + vertical_pos), window = open_buttons[iterate], anchor = 'nw')  
-            iterate += 1
-            endline += 1
+        open_buttons.append(Button(canvas, command = lambda c = iterate: _show(rows[c], rows[c][5], rows[c][6], flag = False), text = "Open Show", width = 13, height = 2, bg = '#000000', fg = '#FFCB09', font = ('sans-serif', 10) , activebackground = '#FFCB09', activeforeground = "#000000" ))
+        canvas.create_window((60 + row_space * endline, 515 + vertical_pos), window = open_buttons[iterate], anchor = 'nw')  
+
+        snooze_buttons.append(Button(canvas, command = lambda c = iterate : snooze(rows[c]), text = 'Snooze', bg ='#554014'))
+        canvas.create_window((200 + row_space * endline, 240 + vertical_pos), window = snooze_buttons[iterate], anchor = 'nw')
+
+        iterate += 1
+        endline += 1
+
 
     canvas.configure(yscrollcommand = scrollbar.set)
     canvas.bind('<Configure>', on_configure)
@@ -335,7 +414,7 @@ def home_page():
     fav_buttons = []
     name = []
     for widget in root.winfo_children():
-        if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button:
+        if widget != discover_button and widget != favorite_button and widget != exit_button and widget != search_button and widget != collection_button:
             widget.destroy()
 
     for i in range(15):
@@ -372,7 +451,3 @@ if response.status_code != 404:
             
 
 root.mainloop()
-
-
-
-
